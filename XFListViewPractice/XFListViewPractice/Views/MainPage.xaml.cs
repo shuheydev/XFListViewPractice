0a030d1c36219ce0363c1a227d8f15c48a76c835
Shuhey <shuhey.dev@gmail.com>
2020-05-30 21:14:07 +0900

    Add project files.

---
+++ XFListViewPractice/XFListViewPractice/Views/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XFListViewPractice.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public List<string> Items { get; set; } = new List<string>();
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Items.AddRange(new string[] { "hello", "wow", "test" });
+
+            BindingContext = this;
+        }
+    }
+}
